74b47319a77a93443b302600f02f2a164def9590
DasGaus <chrisgaus@gmail.com>
2021-04-29 15:52:02 -0400

    work 4/29

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import requests
+import logging
 
 from pages.books import BooksPage
+
+logging.basicConfig()
 
 page_content = requests.get('http://books.toscrape.com/catalogue/page-1.html').content
 page = BooksPage(page_content)
 books = page.books
 
-for i in range(1, 51):
+for i in range(1, page.page_count):
     page_content = requests.get(f'http://books.toscrape.com/catalogue/page-{i}.html').content
     page = BooksPage(page_content)
     books.extend(page.books)
--- pages/books.py
+++ pages/books.py
@@ -1,7 +1,9 @@
+import re
 from bs4 import BeautifulSoup
 
 from locators.book_page_locator import BookPageLocator
 from parsers.book_parser import BookParser
+
 
 class BooksPage:
     def __init__(self, page):
@@ -12,3 +14,11 @@ class BooksPage:
         locator = BookPageLocator.BOOK
         book_tags = self.soup.select(locator)
         return [BookParser(e) for e in book_tags]
+
+    @property
+    def page_count(self):
+        content = self.soup.select_one(BookPageLocator.PAGER).string.strip()
+        pattern = '\w+$'
+        match = re.search(pattern, content)
+        page_int = int(match.group(0))
+        return page_int
